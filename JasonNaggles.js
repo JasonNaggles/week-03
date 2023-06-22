39ec5062a630c25e0e9e1856e0cef95781d5ae0b
Jason Naggles <JasonNaggles.verizon.net>
2023-06-22 00:10:07 -0400

    second commit

---
+++ JasonNaggles.js
@@ -0,0 +1,8 @@
+function lawnTractor (mulchLeaves, baggedLeaves) {
+
+
+    let bagger = baggedLeaves;
+    return bagger;
+}
+    let cleanUp = lawnTractor("leaves", true)
+    console.log(cleanUp);
