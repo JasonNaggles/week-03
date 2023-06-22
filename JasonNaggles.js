5b5186e70ad126f5584a0a31ed3c47f59e0bb7ce
Jason Naggles <JasonNaggles.verizon.net>
2023-06-22 01:05:13 -0400

    edit JasonNaggles.js file

--- JasonNaggles.js
+++ JasonNaggles.js
@@ -1,8 +1,11 @@
+//The lawn tractor mulches leaves & and then they are bagged.
 function lawnTractor (mulchLeaves, baggedLeaves) {
 
-
-    let bagger = baggedLeaves;
+//The bagger the stores the mulched leaves into the bagger.
+   if (bagger = baggedLeaves); {
     return bagger;
+} 
 }
+//The lawn tractor cleaned up all the leaves in the yard.
     let cleanUp = lawnTractor("leaves", true)
     console.log(cleanUp);
